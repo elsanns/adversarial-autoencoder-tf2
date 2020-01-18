d26c42468d73334a7080e1e319a3ee6f5ddcb989
elsanns <3648991+elsanns@users.noreply.github.com>
2020-01-18 16:00:45 +0100

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -10,9 +10,9 @@ def parse_args():
     parser.add_argument('--results_dir', type=str, default='results', help='Training visualization directory')
     parser.add_argument('--log_dir', type=str, default='logs', help='Log directory (Tensorboard)')
     parser.add_argument('--gm_x_stddev', type=float, default=0.5,
-                        help='Gaussian mixture prior: standard dev for the x coord')
+                        help='Gaussian mixture prior: standard deviation for the x coord')
     parser.add_argument('--gm_y_stddev', type=float, default=0.1,
-                        help='Gaussian mixture prior shape: standard dev for the y coord')
+                        help='Gaussian mixture prior shape: standard deviation for the y coord')
     parser.add_argument('--n_epochs', type=int, default=20, help='Number of epochs')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
     parser.add_argument('--batch_size', type=int, default=128, help='Batch size')

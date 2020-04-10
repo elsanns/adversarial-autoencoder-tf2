2265778cde377dcad9e4f666dcd1bafb01d309cc
eliska <3648991+elisans@users.noreply.github.com>
2020-04-10 16:06:33 +0200

    reformatting black flake8

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(name="adversarial-autoencoder-tf2", version="1.0", packages=find_packages())
+setup(name="adversarial-autoencoder-tf2",
+      version="1.0",
+      packages=find_packages())
